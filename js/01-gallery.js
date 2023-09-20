8ae02233ce40c8332426da29810940d17c7db201
Denys <bakhonin1@gmail.com>
2023-09-20 14:49:34 +0200

    homework update 7 (1)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryListImg = document.querySelector('.gallery');
 
-const createGalleryItems = galleryItems.map(({ preview, original, description }) => 
- `<li class="gallery__item">
+const createGalleryItems = galleryItems.reduce((acc, { preview, original, description }) => 
+ acc + `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -16,32 +14,34 @@ const createGalleryItems = galleryItems.map(({ preview, original, description })
     />
   </a>
 </li>`
-);
+, '');
 
-galleryListImg.insertAdjacentHTML('beforeend', createGalleryItems.join(''));
+galleryListImg.insertAdjacentHTML('beforeend', createGalleryItems);
 
 galleryListImg.addEventListener('click', onImgClick);
 
 function onImgClick(evt) {
     evt.preventDefault();
     const { target } = evt;
-    const imgEl = target.dataset.source ?? target.closest(".gallery__image");
+    const imgEl = target.dataset.source;
 
-      if (!target.classList.contains("gallery__image")) {
+    if (!target.classList.contains("gallery__image")) {
         return;
-      }else{
-        const instance = basicLightbox.create(`
+    }
+
+    const instance = basicLightbox.create(`
         <img src="${imgEl}" width="800" height="600">
-    `)
-         
-    instance.show(galleryListImg.addEventListener("keydown", (evt) => {
-      if(evt.code === "Escape"){
-        instance.close();
-        galleryListImg.removeEventListener("keydown", onImgClick);
-      }
-      console.log(evt);
-    }))
-};
+    `);
+
+    instance.show();
+    document.addEventListener("keydown", onCloseEscape);
+
+    function onCloseEscape (evt) {
+        if (evt.code === "Escape") {
+            instance.close();
+            document.removeEventListener("keydown", onCloseEscape);
+        } console.log(evt);
+    };
 }
 
 
